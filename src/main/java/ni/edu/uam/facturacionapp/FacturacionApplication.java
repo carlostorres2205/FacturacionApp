7d679d104c4c8447734fc6907e1cbd18e2e8c2b2
Andy Diaz <asdiazm@uamv.edu.ni>
2026-09-25 16:15:28 -0600

    Create: Application

---
+++ src/main/java/ni/edu/uam/facturacionapp/FacturacionApplication.java
@@ -0,0 +1,28 @@
+package ni.edu.uam.facturacionapp;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class FacturacionApplication extends Application{
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                FacturacionApplication.class.getResource(
+                        "login-view.fxml"
+                )
+        );
+
+        Scene scene = new Scene(
+                fxmlLoader.load()
+        );
+
+        stage.setTitle("Facturación App");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
