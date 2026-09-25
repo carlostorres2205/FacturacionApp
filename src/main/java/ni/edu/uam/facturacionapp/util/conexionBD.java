3e2f26abfe86f7565449540d3c7d9456bac87d11
Andy Diaz <asdiazm@uamv.edu.ni>
2026-09-25 15:51:14 -0600

    Create
    Creando clase conexion, para que exista una conexion con la base de datos.

---
+++ src/main/java/ni/edu/uam/facturacionapp/util/conexionBD.java
@@ -0,0 +1,18 @@
+package ni.edu.uam.facturacionapp.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class conexionBD {
+    private static final String URL =
+            "jdbc:postgresql://localhost:5432/facturacion_db";
+
+    private static final String USUARIO = "postgres";
+
+    private static final String PASSWORD = "Admin2026*";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, PASSWORD);
+    }
+}
