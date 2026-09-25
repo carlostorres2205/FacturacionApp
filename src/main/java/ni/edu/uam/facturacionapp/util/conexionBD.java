89eeeda53a18b6fa0385fef87a2eb8caee1da2bd
Andy Diaz <asdiazm@uamv.edu.ni>
2026-09-25 15:57:59 -0600

    Update: Conexion

--- src/main/java/ni/edu/uam/facturacionapp/util/conexionBD.java
+++ src/main/java/ni/edu/uam/facturacionapp/util/conexionBD.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class conexionBD {
     private static final String URL =
-            "jdbc:postgresql://localhost:5432/facturacion_db";
+            "jdbc:postgresql://localhost:5432/tienda_javafx";
 
     private static final String USUARIO = "postgres";
 
